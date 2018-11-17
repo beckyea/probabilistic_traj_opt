825fddedd79b04b3a89a0e89c0f8ee2928f776c6
mulroyt <32374351+mulroyt@users.noreply.github.com>
2018-11-16 19:45:16 -0500

    updated global variable to show eli how to use git

--- cart_pole_ex/cart_pole_globals.m
+++ cart_pole_ex/cart_pole_globals.m
@@ -1,4 +1,4 @@
-global R Q Q_N E1 D N dof g m L M;
+global R Q Q_N E1 D N dof g m L Mp Mc;
 
 R = .1;
 Q = [10 0; 0 1];
@@ -11,4 +11,5 @@ dof = 2;
 g = 9.81;
 m = 1; %pendulum mass
 L = 1;
-M = 1; %cart mass
+Mc = 1; %cart mass
+Mp = 1; %pend mass
