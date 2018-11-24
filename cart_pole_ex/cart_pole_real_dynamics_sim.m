a9fe66c511a2e68ef20bfc958a20826b3d2514a2
mulroyt <32374351+mulroyt@users.noreply.github.com>
2018-11-24 14:06:38 -0500

    adding in control input by calling dirtrel

--- cart_pole_ex/cart_pole_real_dynamics_sim.m
+++ cart_pole_ex/cart_pole_real_dynamics_sim.m
@@ -1,18 +1,27 @@
 %simulate cartpole dynamics
-%% simulation
+%% compute control with dirtrel
+%call dirtrel
+cart_pole_dirtrel
+%% real dynamics simulation
 cart_pole_globals()
 global Nc_sign
 Nc_sign = 1; %set the sign positive to start
-tspan = [0, 10];
-x0 = [pi/3, 0, 0, 0];
-u = 0;
-% for j = 1:length(traj)
-[t, x] = ode45(@(t,x) compute_cart_pole_dyn(x, t, u), tspan, x0);
-
+tspan = [0, h];
+x0 = [0, 0, 0, 0]; %first initial condition
+allX = [];
+allT = [];
+for j = 1:N-1
+    u = U(j);
+    [t, x] = ode45(@(t,x) compute_cart_pole_dyn(x, t, u), tspan, x0);
+    x0 = x(end, :);
+    tspan = [t(end), t(end) + h];
+    allX = [allX; x];
+    allT = [allT; t];
+end
 %% plotting
-thetas = x(:,1);
-theatadots = x(:,2);
-ys = x(:,3);
-ydots = x(:,4);
+thetas = allX(:,1);
+theatadots = allX(:,2);
+ys = allX(:,3);
+ydots = allX(:,4);
 
-cart_pole_plot(t, thetas, ys)
+cart_pole_plot(allT, thetas, ys)
