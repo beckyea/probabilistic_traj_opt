1fcc01b07e6be20d2c107dceeb553de2087a446a
mulroyt <32374351+mulroyt@users.noreply.github.com>
2018-12-10 20:37:35 -0500

    changing simulate

--- pendulum_ex/LQR_simulate.m
+++ pendulum_ex/LQR_simulate.m
@@ -1,6 +1,6 @@
 x_e0 = [0 0];
 
-global thetas thetadots us dt N Q_N_test
+global thetas thetadots us dt N Q_N_test L m_test K_lqr2 S_lqr2 lqr2
 
 % Define LQR Controller
 Q = [10 0; 0 10];
@@ -12,6 +12,13 @@ thetadots = dirtrel_thetadots;
 us = dirtrel_u;
 dt = h;
 tf = dt * (N-1);
+tf2 = dt * (N-1) + 3;
+
+% Get LQR controller for linearization about the upright
+A_mat = [0, 1; g/L, 0];
+B_mat = [0; 1/(m_test*L^2)];
+[K_lqr2 ,S_lqr2] = lqr(A_mat, B_mat, Q, R);
+lqr2 = 0;
 
 L0sq = chol(Q_N_test)';
 mask = tril(true(2,2));
@@ -23,7 +30,7 @@ t = tf - t;
 t = flip(t);
 Lmat = flipud(Lmat);
 Lmat = spline(t,Lmat');
-[t, x] = ode45(@(t,x) f(x, feedback(t, x, ppval(Lmat, t))), [0 tf], LQR_x_d(0), opts);
+[t, x] = ode45(@(t,x) f(x, feedback(t, x, ppval(Lmat, t))), [0 tf2], LQR_x_d(0), opts);
 
 % get Us from ODE call
 u = zeros(size(t,1)-1,2);
@@ -31,7 +38,7 @@ for i = 1:size(t,1)-1
     u(i,:) = feedback(t(i),x(i,:),ppval(Lmat, t(i)));
 end
     
-
+%pendulum_plot(all_t, x(:,1), us_sim(:,1), 0);
 pendulum_plot(t, x(:,1), u(:,1), 0);
 %pendulum_plot(0:dt:(N-1)*dt, thetas, dirtrel_u)
 
@@ -43,21 +50,30 @@ function Ldotminus = dLdtminus(t,L)
 end
 
 function u = feedback(t, x, Lmat)
-    global R_test m_test
+    global R_test m_test K_lqr2 S_lqr2 lqr2
     pendulum_globals();
-    B = [0 1/(m_test*L*L)]';
-    Lsq = reshape(Lmat,[2 2]);
-    S = Lsq*Lsq';
-    u = LQR_u_d(t);
-%     x_d = LQR_x_d(t)';
-%     x_e = (x-LQR_x_d(t))';
-
-    u_e = -inv(R_test)*B'*S*(x-LQR_x_d(t));
-
-%     disp("x: [" + x(1) + " " + x(2) + "]"  + " x_d: [" + x_d(1) + " " + x_d(2) + "]" + " x_e: [" + x_e(1) + " " + x_e(2) + "]");
-% 
-%     disp("u: " + u + " u_e: " + u_e + " new_u:" + (u+u_e));
-    u = u + u_e;
+    x_star = [pi; 0];
+    if t > tf %(((x - x_star)' * S_lqr2 * (x - x_star)) < 20) || (lqr2 == 1)
+        u = -K_lqr2*(x - x_star);
+        lqr2 = 1;
+        disp("in controller 2");
+    else
+        B = [0 1/(m_test*L*L)]';
+        Lsq = reshape(Lmat,[2 2]);
+        S = Lsq*Lsq';
+        u = LQR_u_d(t);
+        %     x_d = LQR_x_d(t)';
+        %     x_e = (x-LQR_x_d(t))';
+        
+        u_e = -inv(R_test)*B'*S*(x-LQR_x_d(t));
+        
+        %     disp("x: [" + x(1) + " " + x(2) + "]"  + " x_d: [" + x_d(1) + " " + x_d(2) + "]" + " x_e: [" + x_e(1) + " " + x_e(2) + "]");
+        %
+        %     disp("u: " + u + " u_e: " + u_e + " new_u:" + (u+u_e));
+        u = u + u_e;
+    end
+%     all_t = [all_t; t];
+%     us_sim = [us_sim; u(1)];
 end
 
 function xdot = f(xs, us)
@@ -65,7 +81,7 @@ theta = xs(1);
 thetadot = xs(2);
 u = us(1);
 global m_test g L
-xdot = [thetadot; -g/L*sin(theta)+u/(m_test*L*L)];
+xdot = [thetadot; -g/L*sin(theta) + u/(m_test*L*L)];
 % disp("theta: " + theta + " thetadot: " + thetadot + " u: " + u)
 % disp("thetad: " + xdot(1) + " thetadotd: " + xdot(2))
 
--- pendulum_ex/pendulum_globals.m
+++ pendulum_ex/pendulum_globals.m
@@ -4,18 +4,19 @@ R = .1;
 Q = [10 0; 0 1];
 Q_N = 10 * eye(2);
 E1 = [0 0; 0 0];
-D = 0.25;
+D = 0.01;
 
-N = 70; % NUMBER OF KNOT POINTS
+N = 40; % NUMBER OF KNOT POINTS
 
 n_x = 2;
 n_u = 1;
 
 g = 9.81;
-m = 1.25;
+m = 1;
 L = 1;
 
-m_test = .5;
+% randomly sample the mass for the lqr tracking
+m_test = normrnd(m, 0.1);
 Q_test = [500 0; 0 10];
 Q_N_test = [10000 0; 0 10];
 R_test = 0.9;
